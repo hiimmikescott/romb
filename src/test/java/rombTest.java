cb5c9828c0d0d3f7f08d291e05c1255566064c28
Reyra <n.barbara.bordas@gmail.com>
2024-01-10 10:43:08 +0100

    3

--- src/test/java/rombTest.java
+++ src/test/java/rombTest.java
@@ -1,9 +1,16 @@
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import lan.zold.Romb;
 
 public class rombTest {
      Romb romb;
     @BeforeTest
     public void test(){
-        this.romb = new romb();
+        this.romb = new Romb();
+    }
+    @Test
+    public void rombTestCalc(){
+        this.romb.kerulet(30.0);
     }
 }
