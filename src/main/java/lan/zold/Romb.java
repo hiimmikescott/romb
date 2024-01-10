1fe90306fa500099e8587604151547e8bd1b0a81
Reyra <n.barbara.bordas@gmail.com>
2024-01-10 11:20:38 +0100

    9

--- src/main/java/lan/zold/Romb.java
+++ src/main/java/lan/zold/Romb.java
@@ -1,8 +1,14 @@
 package lan.zold;
 
+import java.util.InputMismatchException;
+
 public class Romb {
     public double kerulet(double side, double angle){
-        
+        if ( side <= 0){
+            throw new InputMismatchException();
+        }
+
+
         double rad = angle * Math.PI / 180;
 
         return Math.sqrt(side)*Math.sin(rad);
